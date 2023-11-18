b620994324599de8c1054d102aeedbdf340cd2d6
ATsuStudio <tsyrenovalamga25@gmail.com>
2023-11-18 22:55:08 +0800

    Init

---
+++ src/main/java/com/atsustudio/EdSpringREST/EdSpringRestApplication.java
@@ -0,0 +1,13 @@
+package com.atsustudio.EdSpringREST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdSpringRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdSpringRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atsustudio/EdSpringREST/EdSpringRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atsustudio.EdSpringREST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdSpringRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
